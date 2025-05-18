4162d34d45b6c4ad571fbf108881341b48a7560d
Ahmed Medhat <ahmed.medhat.2029@gmail.com>
2025-05-18 12:45:21 +0300

    Implemented Interfaces and Classes for Domain.Core

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus;
+
+public interface IEventHandler<TEvent> : IEventHandler
+	where TEvent : Event
+{
+	Task Handle(TEvent @event);
+
+	TEvent Handle();
+}
+
+public interface IEventHandler
+{
+
+}

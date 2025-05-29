9b3011921398bfdf65b83d90caaba5f6b9a2cc0e
Ahmed Medhat <ahmed.medhat.2029@gmail.com>
2025-05-29 14:38:44 +0300

    Implemented Publish and SendCommand for Banking Microservice

--- MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,4 +1,5 @@
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,5 +24,13 @@ public class BankingController : ControllerBase
 		_logger.LogInformation("Fetching banking accounts");
 
 		return Ok(_accountService.GetAccounts());
+	}
+
+	[HttpPost]
+	public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+	{
+		_accountService.Transfer(accountTransfer);
+
+		return Ok(accountTransfer);
 	}
 }
